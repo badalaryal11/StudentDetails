34cd857418bf8ba024152f56d5f0a1e7b70cec19
Badal Aryal <badal.aryal@gmail.com>
2022-02-22 17:42:05 +0545

    conditional statement if left blank or null

--- StudentDetails/FrmStudentEntry.cs
+++ StudentDetails/FrmStudentEntry.cs
@@ -16,7 +16,58 @@ namespace StudentDetails
 
             if (string.IsNullOrWhiteSpace(txtName.Text))
             {
-                MessageBox.Show("Enter student details");
+                MessageBox.Show("Enter student name");
+                return;
+            }
+           else if (string.IsNullOrWhiteSpace(txtAddress.Text))
+            {
+                MessageBox.Show("Enter student Address");
+                return;
+            }
+           else if (string.IsNullOrWhiteSpace(txtGender.Text))
+            {
+                MessageBox.Show("Enter student Gender");
+                return;
+            }
+            else if (string.IsNullOrWhiteSpace(txtclass.Text))
+            {
+                MessageBox.Show("Enter student class");
+                return;
+            }
+            else if (string.IsNullOrWhiteSpace(txtRollNo.Text))
+            {
+                MessageBox.Show("Enter student Roll no.");
+                return;
+            }
+            else if (string.IsNullOrWhiteSpace(txtStatus.Text))
+            {
+                MessageBox.Show("Enter student Status");
+                return;
+            }
+           else if (string.IsNullOrWhiteSpace(txtPhone.Text))
+            {
+                MessageBox.Show("Enter student phone");
+                return;
+            }
+
+           else if (string.IsNullOrWhiteSpace(txtNationality.Text))
+            {
+                MessageBox.Show("Enter student Nationality");
+                return;
+            }
+            else if (string.IsNullOrWhiteSpace(txtMother.Text))
+            {
+                MessageBox.Show("Enter student Mother's name");
+                return;
+            }
+           else if (string.IsNullOrWhiteSpace(txtFather.Text))
+            {
+                MessageBox.Show("Enter student Father's Name");
+                return;
+            }
+           else if (string.IsNullOrWhiteSpace(txtDescription.Text))
+            {
+                MessageBox.Show("Enter student Description");
                 return;
             }
 
