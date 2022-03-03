522c671a5e4ee6d59a72075fbc9facd7b27ae667
Badal Aryal <badal.aryal@gmail.com>
2022-03-03 15:18:39 +0545

    sucessfully sorted

--- StudentDetails/FrmStudents.cs
+++ StudentDetails/FrmStudents.cs
@@ -17,8 +17,59 @@ namespace StudentDetails
         {
             InitializeComponent();
             gridStudents.AutoGenerateColumns = false;
+
+            cmbSort.ComboBox.SelectionChangeCommitted += ComboBox_SelectionChangeCommitted;
         }
 
+        private async void ComboBox_SelectionChangeCommitted(object? sender, EventArgs e)
+        {
+            gridStudents.DataSource = null;
+            var studentsList = new List<Student>();
+            var keyword = cmbSort.SelectedItem.ToString().Trim();
+
+            try
+            {
+                using var conn = new SQLiteConnection(@"Data Source=Students.db;Version=3");
+                await conn.OpenAsync();
+                var cmd = new SQLiteCommand($@"SELECT * FROM Student ORDER BY {keyword} ", conn);
+
+
+
+                var reader = await cmd.ExecuteReaderAsync();
+                while (await reader.ReadAsync())
+                {
+                    //gridStudents.DataSource = null;
+                    studentsList.Add(new Student
+                    {
+                        Id = Convert.ToInt32(reader["Id"]),
+                        Name = reader["Name"].ToString(),
+                        Address = reader["Address"].ToString(),
+                        Gender = reader["Gender"]?.ToString() ?? "Not Defined",
+                        Class = reader["Class"].ToString(),
+                        RollNo = Convert.ToInt32(reader["RollNo"]),
+                        Status = reader["Status"].ToString(),
+                        Phone = reader["Phone"].ToString(),
+                        Nationality = reader["Nationality"].ToString(),
+                        MotherName = reader["MotherName"].ToString(),
+                        FatherName = reader["FatherName"].ToString(),
+                        Description = reader["Description"].ToString()
+                    });
+                }
+
+
+                //code executes after loop
+                gridStudents.DataSource = studentsList; // displays datas from list in a datagrid view.
+            }
+            catch (SQLiteException exec)
+            {
+                MessageBox.Show(exec.Message);
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
 
         private async Task LoadDataAsync()
         {
@@ -148,7 +199,7 @@ namespace StudentDetails
                 var reader = await cmd.ExecuteReaderAsync();
                 while (await reader.ReadAsync())
                 {
-                    //gridStudents.DataSource = null;
+
                     studentsList.Add(new Student
                     {
                         Id = Convert.ToInt32(reader["Id"]),
@@ -165,7 +216,7 @@ namespace StudentDetails
                         Description = reader["Description"].ToString()
                     });
 
-                    
+
 
                 }
                 //code executes after loop
@@ -191,62 +242,65 @@ namespace StudentDetails
         private void cmbSort_Click(object sender, EventArgs e)
         {
 
-        }
-    }
-
-
-
-    public abstract class StudentBase
-    {
-    }
-
-    public class Student
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Address { get; set; }
-        public string Gender { get; set; }
-        public string Class { get; set; }
-        public int RollNo { get; set; }
-        public string Status { get; set; }
-        public string Phone { get; set; }
-        public string Nationality { get; set; }
-        public string MotherName { get; set; }
-        public string FatherName { get; set; }
-        public string Description { get; set; }
-
-        public Student(int id, string name, string address, string gender, string @class, int rollno,// intializing an object
-            string status, string phone, string nation, string mother, string father, string describe)
-        {
-            Id = id;
-            Name = name;
-            Address = address;
-            Gender = gender;
-            Class = @class;
-            RollNo = rollno;
-            Status = status;
-            Phone = phone;
-            Nationality = nation;
-            MotherName = mother;
-            FatherName = father;
-            Description = describe;
-        }
-
-        public Student()
-        {
-
-        }
-        public Student(string name, string address)
-        {
-            Name = name;
-            Address = address;
-
-
 
         }
 
     }
 }
+
+
+
+public abstract class StudentBase
+{
+}
+
+public class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public string Gender { get; set; }
+    public string Class { get; set; }
+    public int RollNo { get; set; }
+    public string Status { get; set; }
+    public string Phone { get; set; }
+    public string Nationality { get; set; }
+    public string MotherName { get; set; }
+    public string FatherName { get; set; }
+    public string Description { get; set; }
+
+    public Student(int id, string name, string address, string gender, string @class, int rollno,// intializing an object
+        string status, string phone, string nation, string mother, string father, string describe)
+    {
+        Id = id;
+        Name = name;
+        Address = address;
+        Gender = gender;
+        Class = @class;
+        RollNo = rollno;
+        Status = status;
+        Phone = phone;
+        Nationality = nation;
+        MotherName = mother;
+        FatherName = father;
+        Description = describe;
+    }
+
+    public Student()
+    {
+
+    }
+    public Student(string name, string address)
+    {
+        Name = name;
+        Address = address;
+
+
+
+    }
+
+}
+
 
 
 
